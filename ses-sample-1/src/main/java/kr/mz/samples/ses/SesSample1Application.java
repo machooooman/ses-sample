e0967cd0c66b95347f4c5952da8f9764588b837e
machooooman <user@LAPTOP-125G9TER>
2018-05-21 17:37:06 +0900

    dssss

---
+++ ses-sample-1/src/main/java/kr/mz/samples/ses/SesSample1Application.java
@@ -0,0 +1,12 @@
+package kr.mz.samples.ses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesSample1Application.class, args);
+	}
+}
---
+++ ses-sample-1/src/test/java/kr/mz/samples/ses/SesSample1ApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.mz.samples.ses;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SesSample1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
